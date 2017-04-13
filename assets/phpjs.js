099d81f389c4c70f9855edefe1242449b46f6c54
Hiago Silva Souza <hiago.souza@travelexplorer.com.br>
2017-04-13 12:10:11 -0300

    Added method __isLoaded

--- assets/phpjs.js
+++ assets/phpjs.js
@@ -1,8 +1,15 @@
 var phpjs = (function () {
 	return {
+		__isLoaded: function(name) {
+			return (typeof this[name] != 'undefined')
+		},
 		setup: function(libs) {
 			var self = this;
 			for(var i = 0; i < libs.length; i++) {
+				if(this.__isLoaded(libs[i])) {
+						continue;
+				}
+				
 				var xhttp = new XMLHttpRequest();
 				xhttp.open("GET", 'dist/scripts/'+libs[i]+'.min.js', true);
 				xhttp.send();
@@ -11,7 +18,7 @@ var phpjs = (function () {
 				  if (this.readyState == 4 && this.status == 200) {
 					var name = this.libName;
 					var fn = this.responseText;
-					
+
 					if(fn.substr(0,11) == '"autocall";') {
 						eval(fn.substr(11,fn.length - 10));
 						self[name] = eval(name + "()");
@@ -25,4 +32,3 @@ var phpjs = (function () {
 		}
 	};
 })();
-
